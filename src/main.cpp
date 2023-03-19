d79dda1290885061a3f428ba4517dde834cb9c00
Dmytro Chumak <dimonchumak@gmail.com>
2023-03-19 19:32:29 -0300

    MOV to/from memory

--- src/main.cpp
+++ src/main.cpp
@@ -38,6 +38,7 @@ bool decode (unsigned char * data, int size)
         const uchar byte = p[i];
         if (((byte >> 2) & 0b00111111) == 0b00100010)
         {
+            int extraBytes = 1;
             if (i + 1 >= size)
             {
                 printf("Not enough bytes in instruction stream\n");
@@ -45,42 +46,51 @@ bool decode (unsigned char * data, int size)
             }
 
             const uchar byte2 = p[i+1];
+            const uchar reg = (byte2 & 0b00111000) >> 3;
+            const bool w = (byte & 0b00000001) != 0;
+            const bool d = (byte & 0b00000010) != 0;
+            const char * reg1 = nullptr;
+            // Wide?
+            if (!w)
+            {
+                reg1 = byteRegs[reg];
+            }
+            else
+            {
+                reg1 = wordRegs[reg];
+            }
+
             const uchar mod = (byte2 & 0b11000000) >> 6;
+            const uchar rm = byte2 & 0b00000111;
+            if (rm >= NUM_REGS)
+            {
+                printf("Register index for R/m is too large\n");
+                return false;
+            }
+
             // Register to register
             if (mod == 0b11)
             {
-                const uchar reg = (byte2 & 0b00111000) >> 3;
                 if (reg >= NUM_REGS)
                 {
                     printf("Register index for REG is too large\n");
                     return false;
                 }
 
-                const uchar rm = byte2 & 0b00000111;
-                if (rm >= NUM_REGS)
-                {
-                    printf("Register index for R/m is too large\n");
-                    return false;
-                }
-
-                const char * reg1 = nullptr, * reg2 = nullptr;
-                const bool w = (byte & 0b00000001) != 0;
+                const char * reg2 = nullptr;
                 // Wide?
                 if (!w)
                 {
-                    reg1 = byteRegs[reg];
                     reg2 = byteRegs[rm];
                 }
                 else
                 {
-                    reg1 = wordRegs[reg];
                     reg2 = wordRegs[rm];
                 }
 
                 const char * r1 = 0, * r2 = 0;
 
-                const int d = (byte & 0b00000010) != 0;
-                if (d == 0)
+                if (!d)
                 {
                     // REG=source
                     r1 = reg2;
@@ -96,13 +106,96 @@ bool decode (unsigned char * data, int size)
 
                 printf("mov %s,%s\n", r1, r2);
             }
+            // Memory mode, no displacement
             else
             {
-                printf("Only register to register mode is supported\n");
-                return false;
+                const char * addr = nullptr;
+                switch (rm) {
+                    case 0b000:
+                        addr = "bx + si";
+                        break;
+                    case 0b001:
+                        addr = "bx + di";
+                        break;
+                    case 0b010:
+                        addr = "bp + si";
+                        break;
+                    case 0b011 :
+                        addr = "bp + di";
+                        break;
+                    case 0b100:
+                        addr = "si";
+                        break;
+                    case 0b101:
+                        addr = "di";
+                        break;
+                    case 0b110:
+                        addr = "bp";
+                        break;
+                    case 0b111:
+                        addr = "bx";
+                        break;
+                    default:
+                        break;
+                }
+
+                // mod == 0b00: no displacement
+                ushort disp = 0;
+                // 8-bit displacement
+                if (mod == 0b01)
+                {
+                    if (i + 2 >= size)
+                    {
+                        printf("Not enough bytes in instruction stream\n");
+                        return false;
+                    }
+
+                    const uchar byte3 = p[i + 2];
+                    disp = byte3;
+                    extraBytes += 1;
+                }
+                // 16-bit displacement
+                else if (mod == 0b10)
+                {
+                    if (i + 3 >= size)
+                    {
+                        printf("Not enough bytes in instruction stream\n");
+                        return false;
+                    }
+
+                    const uchar byte3 = p[i + 2];
+                    const uchar byte4 = p[i + 3];
+                    disp = (((ushort)byte4) << 8) | byte3;
+                    extraBytes += 2;
+                }
+
+                if (!d)
+                {
+                    // REG=source
+                    if (disp == 0)
+                    {
+                        printf("mov [%s], %s\n", addr, reg1);
+                    }
+                    else
+                    {
+                        printf("mov [%s + %d], %s\n", addr, disp, reg1);
+                    }
+                }
+                else
+                {
+                    // REG=destination
+                    if (disp == 0)
+                    {
+                        printf("mov %s, [%s]\n", reg1, addr);
+                    }
+                    else
+                    {
+                        printf("mov %s, [%s + %d]\n", reg1, addr, disp);
+                    }
+                }
             }
 
-            i += 2;
+            i += 1 + extraBytes;
         }
         else if (((byte >> 4) & 0b00001111) == 0b00001011)
         {
